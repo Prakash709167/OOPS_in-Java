8db90a5622d05bc1d273c47703cee64e9b572607
Prakash709167 <prakash709167@gmail.com>
2024-01-28 23:24:04 +0530

    commited

---
+++ Leetcode/Question_69.java
@@ -0,0 +1,9 @@
+public class Question_69 {
+    public static double squareroot(double x){
+        return Math.sqrt(x);
+    };
+    public static void main(String mns[]){
+        int x=9;
+        System.out.println(squareroot(x));
+    }
+}
