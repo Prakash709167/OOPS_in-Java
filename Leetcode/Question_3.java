51a40489be1b7e0f539647a9765e76187cd58fc2
Prakash709167 <prakash709167@gmail.com>
2024-02-06 22:12:20 +0530

    commited

---
+++ Leetcode/Question_3.java
@@ -0,0 +1,15 @@
+public class Question_3 {
+    public static void substring(String str){
+        String ans = " ";
+        String result = " ";
+        int i =0;
+        //base case
+        if(i==str.length()){
+            System.out.println(ans);
+            return;
+        }
+        //recursion
+        //yes choice
+        substring(str);
+    }
+}
