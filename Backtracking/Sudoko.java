b05f4b30acffa1bc396a7cf3153082a2d9656202
Prakash709167 <prakash709167@gmail.com>
2024-02-12 19:11:22 +0530

    commited

---
+++ Backtracking/Sudoko.java
@@ -0,0 +1,81 @@
+public class Sudoko {
+    public static boolean isSafe(int sudu[][], int row, int col, int digit){
+        //col
+        for(int i=0;i<=8;i++){
+            if(sudu[i][col]==digit){
+                return false;
+            }
+        }
+        //row
+        for(int j =0;j<=8;j++){
+            if(sudu[row][j]==digit){
+                return false;
+            }
+        }
+        //grid
+        int sr = (row/3) * 3;
+        int sc = (col/3) * 3;
+        for(int i = sr; i<sr+3; i++){
+            for(int j = sc; j<sc+3; j++){
+                if(sudu[i][j]==digit){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+    public static boolean sudokoSolver(int sudu[][], int row, int col){
+        //base case
+        if(row == 9 && col == 0){
+            return true;
+        }
+        //recursion
+        int nextRow = row, nextCol = col+1;
+        if(col+1==9){
+            nextRow = row+1;
+            nextCol=0;
+        }
+        if(sudu[row][col]!=0){
+            return sudokoSolver(sudu,nextRow,nextCol);
+        }
+        for(int digit=1;digit<=9;digit++){
+            if(isSafe(sudu,row,col,digit)){
+                sudu[row][col]=digit;
+                if(sudokoSolver(sudu,nextRow,nextCol)){
+                    return true;
+                }
+                sudu[row][col] = 0;
+            }
+        }
+        return false;
+    }
+    public static void printSudoko(int sudu[][]){
+        for(int row=0;row<=8;row++){
+            for(int col = 0; col<=8;col++){
+                System.out.print(sudu[row][col]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int sudu[][]= 
+        {{0,0,8,0,0,0,0,0,0},
+        {4,9,0,1,5,7,0,0,0},
+        {0,0,3,0,0,4,1,9,0},
+        {1,8,5,0,6,0,0,2,0},
+        {0,0,0,0,2,0,0,6,0},
+        {9,6,0,4,0,5,3,0,0},
+        {0,3,0,0,7,2,0,0,4},
+        {0,4,9,0,3,0,0,5,7},
+        {8,2,7,0,0,9,0,1,3}};
+        if(sudokoSolver(sudu,0,0)){
+            System.out.println("Solution exists");
+            printSudoko(sudu);
+        }
+        else{
+            System.out.println("Solution does not exist");
+        }
+
+    }
+}
