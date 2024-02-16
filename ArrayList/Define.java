0c4930151cc6899bc2c394df72ad8b7548d0d7d3
Prakash709167 <prakash709167@gmail.com>
2024-02-16 22:02:31 +0530

    commited

--- ArrayList/Define.java
+++ ArrayList/Define.java
@@ -6,10 +6,10 @@ public class Define {
         //Operations
         //1. add element
         // System.out.println("add element");
-        List.add(1);
-        List.add(2);
+        List.add(8);
+        List.add(5);
+        List.add(12);
         List.add(3);
-        List.add(4);
         // System.out.println(List);
 
         //2.get element
@@ -42,9 +42,32 @@ public class Define {
         System.out.println();
 
         //8. Reverse of ArrayList
-        for(int i = List.size()-1;i>=0;i--){
-            System.out.print(List.get(i)+" ");
-        }
-        System.out.println();
+        // for(int i = List.size()-1;i>=0;i--){
+        //     System.out.print(List.get(i)+" ");
+        // }
+        // System.out.println();
+        
+        //9. Largest value
+        // int max = Integer.MIN_VALUE;
+        // for(int i =0;i <List.size();i++){
+        //     max = Math.max(max,List.get(i));
+        // }
+        // System.out.println(max);
+
+        //10. Swap 2 number by method
+        // Swapping(List,1,3);
+
+        //11. Sort ArrayList
+        Collections.sort(List);//accesnding order
+        System.out.println(List);
+        Collections.sort(List,Collections.reverseOrder());//decending operator
+        System.out.println(List);
+        
+    }
+    public static void Swapping(ArrayList<Integer> list , int index1,int index2){
+        int temp = list.get(index1);
+        list.set(index1,list.get(index2));
+        list.set(index2,temp);
+        System.out.println(list);
     }
 }
