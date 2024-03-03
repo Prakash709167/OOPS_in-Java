795574217e4264b2069c6e683d3a76809cbd53d4
Prakash709167 <prakash709167@gmail.com>
2024-03-03 19:33:01 +0530

    commited

---
+++ Queue/QueueArr.java
@@ -0,0 +1,64 @@
+public class QueueArr {
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear;
+        Queue(int n){
+            arr = new int[n];
+            size = n;
+            rear = -1;
+        }
+        public boolean isEmpty(){
+            return rear == -1;
+        }
+
+        //adding element in queue
+
+        public void add(int data){
+            if(rear == size-1){
+                System.out.println("Queue is full");
+            }
+            rear = rear+1;
+            arr[rear]=data;
+        }
+
+        //removeing element from queue
+
+        public int remove(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int front = arr[0];
+            for(int i =0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear = rear-1;
+            return front;
+        }
+
+        // peek value returning
+
+        public int peek(){
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+    public static void main(String args[]){
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        // q.add(6);
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+        q.peek();
+    }
+}
