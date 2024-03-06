dbc5669ca489893187c49def8631c29ed1a695be
Prakash709167 <prakash709167@gmail.com>
2024-03-07 00:13:44 +0530

    commited

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -3,22 +3,32 @@ public class CircularQueue {
         static int arr[];
         static int size;
         static int rear;
+        static int front;
         Queue(int n){
             arr = new int[n];
             size = n;
             rear=-1;
+            front = -1;
         }
         // isEmpty
         public boolean isEmpty(){
-            return rear==-1;
+            return rear==-1 && front == -1;
+        }
+        //isfull
+        public boolean isfull(){
+            return (rear+1)%size == front;
         }
         //adding data
         public void add(int data){
-            if(rear == size-1){
+            if(isfull()){
                 System.out.println("Queue is full");
                 return;
             }
-            rear = rear+1;
+            //add first element
+            if(front==-1){
+                front = 0;
+            }
+            rear = (rear+1)%size;
             arr[rear] = data;
         }
         //removing data
@@ -27,19 +37,22 @@ public class CircularQueue {
                 System.out.println("Queue is empty");
                 return -1;
             }
-            int front = arr[0];
-            for(int i = 0;i<rear;i++){
-                arr[i]=arr[i+1];
+            int result = arr[front];
+            //last element of queue
+            if(rear==front){
+                front = rear =-1;
             }
-            rear = rear-1;
-            return front;
+            else{
+                front = (front+1)%size;
+            }
+            return result;
         }
         public int peek(){
             if(isEmpty()){
                 System.out.println("Queue is Empty");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
     }
     public static void main(String args[]){
@@ -49,10 +62,17 @@ public class CircularQueue {
         q.add(3);
         q.add(4);
         q.add(5);
+        System.out.println(q.remove());
+        q.add(6);
+        System.out.println(q.remove());
+        q.add(7);
+        q.add(8);
         while(!q.isEmpty()){
             System.out.print(q.peek()+" | ");
             q.remove();
         }
-        System.out.println(q.peek());
+        System.out.println();
+        q.remove();
+        // System.out.println(q.peek());
     }
 }
